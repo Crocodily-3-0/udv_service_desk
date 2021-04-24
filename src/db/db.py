e33a48127b7870cce989e1155855d0ffe201b999
Puzanovim <puzanovim@yandex.ru>
2021-04-24 20:09:50 +0500

    add a db connection

---
+++ src/db/db.py
@@ -0,0 +1,15 @@
+import databases
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./pre-production.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./production.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+database = databases.Database(SQLALCHEMY_DATABASE_URL)
+Base: DeclarativeMeta = declarative_base()
