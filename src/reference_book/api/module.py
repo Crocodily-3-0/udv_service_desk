62f6cef46a857ced33432ebe322a5cde6130672f
Puzanovim <puzanovim@yandex.ru>
2021-04-25 21:11:01 +0500

    add status code

--- src/reference_book/api/module.py
+++ src/reference_book/api/module.py
@@ -1,32 +1,32 @@
 from typing import List
 
-from fastapi import APIRouter
-from ..schemas import Module, ModuleShort, ModuleCreate
+from fastapi import APIRouter, status
+from ..schemas import Module, ModuleShort, ModuleCreate, ModuleDB
 from ..services import get_module, get_modules, add_module, delete_module, update_module
 
 router = APIRouter()
 
 
-@router.get("/", response_model=List[ModuleShort])
+@router.get("/", response_model=List[ModuleShort], status_code=status.HTTP_200_OK)
 async def module_list():
     return await get_modules()
 
 
-@router.get('/{id}', response_model=Module)
+@router.get('/{id}', response_model=Module, status_code=status.HTTP_200_OK)
 async def module(id: int):
     return await get_module(id)
 
 
-@router.post("/")
+@router.post("/", response_model=ModuleDB, status_code=status.HTTP_201_CREATED)
 async def create_module(item: ModuleCreate):
     return await add_module(item)
 
 
-@router.delete("/{id}")
-async def delete_module(id: int):
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_module_by_id(id: int):
     return await delete_module(id)
 
 
-@router.put("/{id}")
-async def update_module(id: int, item: ModuleCreate):
+@router.put("/{id}", response_model=ModuleDB, status_code=status.HTTP_201_CREATED)
+async def update_module_by_id(id: int, item: ModuleCreate):
     return await update_module(id, item)
--- src/reference_book/api/software.py
+++ src/reference_book/api/software.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+
 from ..services import get_software, get_softwares, get_software_with_modules, \
     add_software, delete_software, update_software
 from ..schemas import Software, SoftwareShort, SoftwareCreate
@@ -8,31 +9,31 @@ from ..schemas import Software, SoftwareShort, SoftwareCreate
 router = APIRouter()
 
 
-@router.get('/', response_model=List[SoftwareShort])
+@router.get('/', response_model=List[SoftwareShort], status_code=status.HTTP_200_OK)
 async def software_list():
     return await get_softwares()
 
 
-@router.get('/{id}', response_model=SoftwareShort)
+@router.get('/{id}', response_model=SoftwareShort, status_code=status.HTTP_200_OK)
 async def software(id: int):
     return await get_software(id)
 
 
-@router.get("/{id}/modules", response_model=Software)
+@router.get("/{id}/modules", response_model=Software, status_code=status.HTTP_200_OK)
 async def get_software_modules(id: int):
     return await get_software_with_modules(id)
 
 
-@router.post("/")
-async def create_licence(software: SoftwareCreate):
+@router.post("/", response_model=SoftwareShort, status_code=status.HTTP_201_CREATED)
+async def create_software(software: SoftwareCreate):
     return await add_software(software)
 
 
-@router.delete("/{id}")
-async def delete_software(id: int):
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_software_by_id(id: int):
     return await delete_software(id)
 
 
-@router.put("/{id}")
-async def update_software(id: int, item: SoftwareCreate):
+@router.put("/{id}", response_model=SoftwareShort, status_code=status.HTTP_201_CREATED)
+async def update_software_by_id(id: int, item: SoftwareCreate):
     return await update_software(id, item)
