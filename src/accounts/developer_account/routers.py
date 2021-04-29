a22e9b1c53e523083389dd84a6b0f0c3765f756f
Puzanovim <puzanovim@yandex.ru>
2021-04-29 13:33:42 +0500

    add developers account

---
+++ src/accounts/developer_account/routers.py
@@ -0,0 +1,36 @@
+from typing import List
+
+from fastapi import APIRouter, Depends, status, Response
+from pydantic.types import UUID4
+
+from .services import get_developers, get_developer, add_developer, update_developer, delete_developer
+from src.users.models import UserTable
+from src.users.logic import developer_user
+from src.users.schemas import UserDB, UserCreate, UserUpdate
+
+developer_router = APIRouter()
+
+
+@developer_router.get("/", response_model=List[UserDB], status_code=status.HTTP_200_OK)
+async def developers_list(user: UserTable = Depends(developer_user)):
+    return await get_developers()
+
+
+@developer_router.get("/{id:uuid}", response_model=UserDB, status_code=status.HTTP_200_OK)
+async def developer(id: UUID4, user: UserTable = Depends(developer_user)):
+    return await get_developer(id)
+
+
+@developer_router.post("/")
+async def create_developer(item: UserCreate, user: UserTable = Depends(developer_user)):
+    return await add_developer(item)
+
+
+@developer_router.patch("/{id:uuid}", response_model=UserDB, status_code=status.HTTP_201_CREATED)
+async def update_developer_by_id(id: UUID4, item: UserUpdate, user: UserTable = Depends(developer_user)):
+    return await update_developer(id, item)
+
+
+@developer_router.delete("/{id:uuid}", response_class=Response, status_code=status.HTTP_204_NO_CONTENT)
+async def delete_developer_by_id(id: UUID4, user: UserTable = Depends(developer_user)):
+    await delete_developer(id)
---
+++ src/accounts/developer_account/services.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+from fastapi import Request, HTTPException, status
+from fastapi_users.router import ErrorCode
+from pydantic.types import UUID4
+
+from src.db.db import database
+from src.users.logic import all_users, delete_user, update_user
+from src.users.models import users
+from src.users.schemas import UserCreate, DeveloperCreate, UserUpdate
+
+
+async def get_developers():
+    developers_data = await database.fetch_all(users.select().where(users.c.is_superuser is True))
+    return [dict(developer) for developer in developers_data]
+
+
+async def get_developer(id: UUID4):
+    developer = await database.fetch_one(users.select().where((users.c.is_superuser is True) & (users.c.id == id)))
+    if developer:
+        developer = dict(developer)
+        return developer
+    return None
+
+
+async def add_developer(user: UserCreate):
+    developer = DeveloperCreate(
+        **user.dict(),
+        date_reg=datetime.utcnow())
+    try:
+        created_developer = await all_users.create_user(developer, safe=True)
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=ErrorCode.REGISTER_USER_ALREADY_EXISTS,
+        )
+
+    return created_developer
+
+
+async def update_developer(id: UUID4, item: UserUpdate):
+    update_dict = item.dict(exclude_unset=True)
+    updated_developer = await update_user(id, update_dict)
+    return updated_developer
+
+
+async def delete_developer(id: UUID4):
+    await delete_user(id)
