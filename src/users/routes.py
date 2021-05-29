cfc6fe60923c2adfaa7b26eeff8ff36b6e829fa3
Puzanovim <puzanovim@yandex.ru>
2021-05-29 20:50:16 +0500

    update user/me

--- src/users/routes.py
+++ src/users/routes.py
@@ -1,9 +1,11 @@
 from fastapi import Depends, Response
 from fastapi import APIRouter
+
+from .schemas import UserDB
 from ..config import SECRET
 
 from src.users.logic import jwt_authentication, all_users, \
-    on_after_forgot_password, on_after_reset_password, after_verification, after_verification_request
+    on_after_forgot_password, on_after_reset_password, after_verification, after_verification_request, any_user
 
 router = APIRouter()
 
@@ -11,6 +13,13 @@ router = APIRouter()
 @router.post("/auth/jwt/refresh")
 async def refresh_jwt(response: Response, user=Depends(all_users.get_current_active_user)):
     return await jwt_authentication.get_login_response(user, response)
+
+
+@router.get("/me", response_model=UserDB)
+async def me(
+    user: UserDB = Depends(any_user),  # type: ignore
+):
+    return user
 
 
 router.include_router(
@@ -28,10 +37,11 @@ router.include_router(
         after_reset_password=on_after_reset_password),
     prefix="/auth",
     tags=["auth"])
-router.include_router(
-    all_users.get_users_router(),
-    prefix="/users",
-    tags=["users"])
+# router.include_router(
+#     all_users.get_users_router(),
+#     prefix="/users",
+#     tags=["users"])
+
 router.include_router(
     all_users.get_verify_router(
         SECRET,
