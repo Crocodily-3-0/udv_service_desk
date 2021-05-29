a3e7c72c9b14223e820817654400b31817ba42d7
Puzanovim <puzanovim@yandex.ru>
2021-05-29 20:53:30 +0500

    add CORS

--- src/app.py
+++ src/app.py
@@ -1,15 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.db.db import database, engine
 from .db.base import Base
 from .accounts.api import accounts_router
 from .desk.routes import router as desk_router
-from .desk.dev_routes import dev_router
 from .reference_book.api.routes import router as book_router
 from .users.routes import router as users_routes
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event('startup')
@@ -30,5 +42,4 @@ def read_root():
 app.include_router(accounts_router)
 app.include_router(book_router, prefix='/references', tags=['Reference book'])
 app.include_router(desk_router, prefix='/desk', tags=['Desk'])
-app.include_router(dev_router, prefix='/desk', tags=['Desk'])
 app.include_router(users_routes)
